9db48a61cb3b13c6d62c47d2c9b01aeb99e3d543
Stevens <stevens.bideau@orange.fr>
2024-08-06 23:59:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,14 +91,11 @@ def display_movies(movies):
                     </div>
                     """, unsafe_allow_html=True)
 
-
-
-@st.experimental_memo
+@st.cache_data
 def get_img_as_base64(file):
     with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
-
 
 img = get_img_as_base64("image.jpg")
 
