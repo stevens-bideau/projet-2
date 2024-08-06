4759860d07e265d22aa80d9cae85d88a1cebc2e0
Stevens <stevens.bideau@orange.fr>
2024-08-06 17:17:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,6 @@ import numpy as np
 
 
 # Intégrer le CSS pour l'image de fond
-#background_image_url = "https://www.phipix.com/protojam/bg_minigroot.jpg"  # Remplacez par l'URL de votre image
 background_image_url = "https://www.phipix.com/data_projet2/banniere-reco-cine-creuse.png"  # Remplacez par l'URL de votre image
 page_bg_img = f"""
 <style>
@@ -17,13 +16,10 @@ page_bg_img = f"""
     background-position: center top 60px; /* caller l'image en centré en haut (top) */
     background-repeat: no-repeat; /* ne pas répéter l'image en mosaïac */
     background-color: #262730; /* couleur de fons */
-
 }}
-
 </style>
 """
 st.markdown(page_bg_img, unsafe_allow_html=True)
-
 
 # Charger les données depuis l'URL
 url = 'https://www.phipix.com/data_projet2/df_final_ml.csv'
@@ -81,7 +77,6 @@ def find_similar_movies(movie_title, knn, df, final_features, n_neighbors=10):
     similar_movies['distance'] = distances[0]
     return similar_movies
 
-
 # Interface Streamlit
 
 # Titre de l'application // Ciné Creuse - Recommandation de films
@@ -89,43 +84,49 @@ st.title(' ')
 st.title(' ')
 st.title(' ')
 
-# Container fixe / search zone
-placeholder = st.empty()
+# Container fixe pour la zone de saisie de texte
+search_container = st.container()
+result_container = st.container()
+selection_container = st.container()
 
-# Entrée utilisateur avec suggestions automatiques
-selected_title = st.selectbox('Recherche un film que vous aimez :', df_ml_reco['title'])
+with search_container:
+    # Entrée utilisateur avec suggestions automatiques
+    selected_title = st.selectbox('Recherche un film que vous aimez :', df_ml_reco['title'])
 
-placeholder.empty()
-
-
-# Bouton pour lancer la recherche
 if selected_title:
-    similar_movies = find_similar_movies(selected_title, knn, df_ml_reco, final_features)
-    if similar_movies is not None:
-        st.write(f'Films similaires à: {selected_title}:')
+    with result_container:
+        similar_movies = find_similar_movies(selected_title, knn, df_ml_reco, final_features)
+        if similar_movies is not None:
+            st.write(f'Films similaires à: {selected_title}:')
 
-        # Nombre de colonnes par ligne
-        num_columns_per_row = 5
+            # Exclure le film sélectionné des résultats
+            similar_movies = similar_movies[similar_movies['title'] != selected_title]
 
-        # Créez des colonnes pour chaque ligne
-        num_movies = len(similar_movies)
-        num_rows = (num_movies + num_columns_per_row - 1) // num_columns_per_row
-        
-        for row in range(num_rows):
-            cols = st.columns(num_columns_per_row)
-            for i in range(num_columns_per_row):
-                movie_index = row * num_columns_per_row + i
-                if movie_index < num_movies:
-                    movie = similar_movies.iloc[movie_index]
-                    with cols[i]:
-                        # Créez l'URL de l'image
-                        image_url = 'https://image.tmdb.org/t/p/original' + movie['poster_path']
-                        st.markdown(f"""
-                        <div style="text-align: center;">
-                            <img src="{image_url}" width="100" style="border-radius: 8px;">
-                            <div style="text-align: center; line-height: 1.2; margin-top: 5px;">
-                                <strong>{movie['title']}</strong><br>
-                                Année : {movie['year']}<br>
+            # Nombre de colonnes par ligne
+            num_columns_per_row = 5
+
+            # Créez des colonnes pour chaque ligne
+            num_movies = len(similar_movies)
+            num_rows = (num_movies + num_columns_per_row - 1) // num_columns_per_row
+            
+            for row in range(num_rows):
+                cols = st.columns(num_columns_per_row)
+                for i in range(num_columns_per_row):
+                    movie_index = row * num_columns_per_row + i
+                    if movie_index < num_movies:
+                        movie = similar_movies.iloc[movie_index]
+                        with cols[i]:
+                            # Créez l'URL de l'image
+                            image_url = 'https://image.tmdb.org/t/p/original' + movie['poster_path']
+                            st.markdown(f"""
+                            <div style="text-align: center;">
+                                <img src="{image_url}" width="100" style="border-radius: 8px;">
+                                <div style="text-align: center; line-height: 1.2; margin-top: 5px;">
+                                    <strong>{movie['title']}</strong><br>
+                                    Année : {movie['year']}<br>
+                                </div>
                             </div>
-                        </div>
-                        """, unsafe_allow_html=True)                       
+                            """, unsafe_allow_html=True)
+
+with selection_container:
+    st.markdown("## Notre sélection")
