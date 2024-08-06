066989cda6074b2e65b203ee1d10006756344d76
Stevens <stevens.bideau@orange.fr>
2024-08-07 00:12:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -97,7 +97,7 @@ def get_img_as_base64(file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img = get_img_as_base64("banniere-reco-cine-creuse.png")
+
 
 page_bg_img = f"""
 <style>
