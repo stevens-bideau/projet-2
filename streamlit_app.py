f766e8a0733c5c546a5a80ff01740f425c28b52e
Stevens <stevens.bideau@orange.fr>
2024-08-07 13:48:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,40 +176,45 @@ with selection_container:
         
     with tabs[1]:
         st.header("Mystère")
-        history_movies = df_ml_reco[df_ml_reco['Mystery'] == 1].sample(n=10)
+        mystery_movies = df_ml_reco[df_ml_reco['Mystery'] == 1].sample(n=10)
         display_movies(mystery_movies) 
 
     with tabs[2]:
         st.header("Familiale")
-        drama_movies = df_ml_reco[df_ml_reco['Family'] == 1].sample(n=10)
+        family_movies = df_ml_reco[df_ml_reco['Family'] == 1].sample(n=10)
         display_movies(family_movies)
 
     with tabs[3]:
         st.header("Historique")
-        action_movies = df_ml_reco[df_ml_reco['History'] == 1].sample(n=10)
+        history_movies = df_ml_reco[df_ml_reco['History'] == 1].sample(n=10)
         display_movies(history_movies)
 
     with tabs[4]:
         st.header("Biographique")
-        comedy_movies = df_ml_reco[df_ml_reco['Biography'] == 1].sample(n=10)
+        biography_movies = df_ml_reco[df_ml_reco['Biography'] == 1].sample(n=10)
         display_movies(biography_movies)
 
     with tabs[5]:
         st.header("Drame")
-        history_movies = df_ml_reco[df_ml_reco['Drama'] == 1].sample(n=10)
+        drama_movies = df_ml_reco[df_ml_reco['Drama'] == 1].sample(n=10)
         display_movies(drama_movies) 
 
     with tabs[6]:
         st.header("Western")
-        drama_movies = df_ml_reco[df_ml_reco['Western'] == 1].sample(n=10)
+        western_movies = df_ml_reco[df_ml_reco['Western'] == 1].sample(n=10)
         display_movies(western_movies)
 
     with tabs[7]:
+        st.header("Guerre")
+        war_movies = df_ml_reco[df_ml_reco['War'] == 1].sample(n=10)
+        display_movies(war_movies)
+
+    with tabs[8]:
         st.header("Action")
         action_movies = df_ml_reco[df_ml_reco['Action'] == 1].sample(n=10)
         display_movies(action_movies)
 
-    with tabs[8]:
+    with tabs[9]:
         st.header("Comédie")
         comedy_movies = df_ml_reco[df_ml_reco['Comedy'] == 1].sample(n=10)
         display_movies(comedy_movies)
