7a3fdfb4efb5ea780bb84057ef67a9e9312aa224
Stevens <stevens.bideau@orange.fr>
2024-08-08 12:42:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,8 +103,8 @@ def display_movies(movies, tab_name=""):
 def button_dialog(item, image_url):
     movie = df_ml_reco.loc[df_ml_reco['tconst']==item]
     st.image(image_url, width=300)
-    st.write(f"**Titre :** {movie['title']}")
-    st.write(f"**Année :** {movie['year']}")
+    st.write(f"**Titre :** {movie['title'][1]}")
+    st.write(f"**Année :** {movie['year'][1]}")
     st.write(f"**Runtime :** {movie.get('runtime', 'N/A')}")
     st.write(f"**Average Rating :** {movie.get('averageRating', 'N/A')}")
     st.write(f"**Number of Votes :** {movie.get('numVotes', 'N/A')}")
